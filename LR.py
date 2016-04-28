24d7341fc36a4c4bf31c12bc2e9dbd074f95aa66
tianxin <tianxin1860@gmail.com>
2016-04-28 17:23:23 +0800

    source code for LR model

---
+++ LR.py
@@ -0,0 +1,145 @@
+
+
+from datetime import datetime
+from csv import DictReader
+from math import exp, log, sqrt
+
+
+# parameters #################################################################
+
+train = 'train.csv'  # path to training file
+test = 'test.csv'  # path to testing file
+model = 'LR.model'
+
+D = 2 ** 20   # demension of weights use for learning
+alpha = .1    # learning rate for sgd optimization
+w = [0.] * D  # weight
+n = [0.] * D  # number of times we've encountered a feature(n for compute adaptive learning rate)
+
+
+# function definitions #######################################################
+
+# A. Bounded logloss
+# INPUT:
+#     p: our prediction
+#     y: real answer
+# OUTPUT
+#     logarithmic loss of p given y
+def logloss(p, y):
+    p = max(min(p, 1. - 10e-12), 10e-12)
+    return -log(p) if y == 1. else -log(1. - p)
+
+
+# B. Apply hash trick of the original csv row
+# for simplicity, we treat both integer and categorical features as categorical
+# INPUT:
+#     csv_row: a csv dictionary, ex: {'Lable': '1', 'I1': '357', 'I2': '', ...}
+#     D: the max index that we can hash to
+# OUTPUT:
+#     x: a list of indices that its value is 1
+def get_x(csv_row, D):
+    x = [0]  # 0 is the index of the bias term
+    for key, value in csv_row.items():
+        index = int(value + key[1:], 16) % D  # weakest hash ever ;)
+        x.append(index)
+    return x  # x contains indices of features that have a value of 1
+
+
+# C. Get probability estimation on x
+# INPUT:
+#     x: features
+#     w: weights
+# OUTPUT:
+#     probability of p(y = 1 | x; w)
+def get_p(x, w):
+    wTx = 0.
+    for i in x:  # do wTx
+        wTx += w[i] * 1.  # w[i] * x[i], but if i in x we got x[i] = 1.
+    return 1. / (1. + exp(-max(min(wTx, 20.), -20.)))  # bounded sigmoid
+
+
+# D. Update given model
+# INPUT:
+#     w: weights
+#     n: a counter that counts the number of times we encounter a feature
+#        this is used for adaptive learning rate
+#     x: feature
+#     p: prediction of our model
+#     y: answer
+# OUTPUT:
+#     w: updated model
+#     n: updated count
+def update_w(w, n, x, p, y):
+    for i in x:
+        # alpha / (sqrt(n) + 1) is the adaptive learning rate heuristic
+        # (p - y) * x[i] is the current gradient
+        # note that in our case, if i in x then x[i] = 1
+        w[i] -= (p - y) * alpha / (sqrt(n[i]) + 1.)
+        n[i] += 1.
+    return w, n
+
+
+# training and testing #######################################################
+
+# initialize our model
+
+# start training a logistic regression model using on pass sgd
+
+
+def train_model(train_file):
+    global w, n
+    cost = 0.0
+    for t, row in enumerate(DictReader(open(train_file))):
+        y = 1. if row['Label'] == '1' else 0.
+
+        del row['Label']  # can't let the model peek the answer
+        del row['Id']  # we don't need the Id
+
+        # main training procedure
+        # step 1, get the hashed features
+        x = get_x(row, D)
+
+        # step 2, get prediction
+        p = get_p(x, w)
+
+        # for progress validation, useless for learning our model
+        cost += logloss(p, y)
+        if t % 10000 == 0 and t > 1:
+            print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), t, cost/t))
+
+        # step 3, update model with answer
+        w, n = update_w(w, n, x, p, y)
+    with open('LR.model','w') as model:
+        model.write("%s\n" % str(w) )
+        model.write("%s\n" % str(n) )
+
+
+
+# testing (build kaggle's submission file)
+def predict(test_file):
+    print("predict result")
+    cost = 0.0
+    with open('submission_my_20.csv', 'w') as submission:
+        submission.write('Id,Predicted\n')
+        for t, row in enumerate(DictReader(open(test_file))):
+            y = 1. if row['Label'] == '1' else 0.
+            Id = row['Id']
+            del row['Id']
+            del row['Label']
+            x = get_x(row, D)
+            p = get_p(x, w)
+            cost += logloss(p, y)
+            submission.write('%s,%f\n' % (Id, p))
+        if(t % 10000 == 0):
+            print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), t, cost/t))
+    print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), t, cost/t))
+
+#def load_model(model_file):
+#    with open(model_file) as model:
+#        for line in model:
+#            print(line)
+
+if __name__ == '__main__':
+    #load_model(model)
+    train_model(train)
+    predict(test)
